c1d736c111cd97c9304416a65843bc7834feb315
mack13UW <mack13@uw.edu>
2019-09-23 15:13:55 -0700

    Add files via upload

--- Model-Specs.R
+++ Model-Specs.R
@@ -24,7 +24,45 @@ v$fitindices_robust <- c("npar", "chisq.scaled", "df.scaled", "cfi.scaled", "tli
 
 # Model designations using Lavaan format
 m1 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1' ### m1 is original model with no correlated residuals
-m2 <- 'F1 =~ ctar_q2_1 + ctar_q6_1 + ctar_q8_1 + ctar_q9_1 + ctar_q15_1 + ctar_q16_1
+m2 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1
+      
+      # Correlated residuals
+      ctar_q14_1 ~~ ctar_q16_1'
+
+m3 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1
+      ctar_q14_1 ~~ ctar_q16_1
+      ctar_q13_1 ~~ ctar_q14_1' 
+
+m4 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1
+      
+      # Correlated residuals
+      ctar_q14_1 ~~ ctar_q16_1
+      ctar_q13_1 ~~ ctar_q14_1
+      ctar_q13_1 ~~ ctar_q16_1' 
+
+m5 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1
+      
+      # Correlated residuals
+      ctar_q14_1 ~~ ctar_q16_1
+      ctar_q13_1 ~~ ctar_q14_1
+      ctar_q13_1 ~~ ctar_q16_1
+
+      # Constrained thresholds for single item (all)
+      ctar_q17_1 | c(a11,a11)*t1; ctar_q17_1 | c(b11,b11)*t2; ctar_q17_1 | c(c11,c11)*t3'
+
+m6 <- 'F1 =~ ctar_q1_1 + ctar_q2_1 + ctar_q3_1 + ctar_q4_1 + ctar_q5_1 + ctar_q6_1 + ctar_q7_1 + ctar_q8_1 + ctar_q9_1 + ctar_q10_1 + ctar_q11_1 + ctar_q12_1 + ctar_q13_1 + ctar_q14_1 + ctar_q15_1 + ctar_q16_1 + ctar_q17_1\n      ctar_q14_1 ~~ ctar_q16_1\n      ctar_q13_1 ~~ ctar_q14_1\n      ctar_q13_1 ~~ ctar_q16_1; 
+      
+      # Correlated residuals
+      ctar_q14_1 ~~ ctar_q16_1
+      ctar_q13_1 ~~ ctar_q14_1
+      ctar_q13_1 ~~ ctar_q16_1
+
+      # Constrained thresholds for single item (one)
+      ctar_q1_1 | c(a11,a11)*t1; ctar_q1_1 | c(b11,b12)*t2; ctar_q1_1 | c(c11,c12)*t3'
+
+m7 <- 'F1 =~ ctar_q2_1 + ctar_q6_1 + ctar_q8_1 + ctar_q9_1 + ctar_q15_1 + ctar_q16_1'
+
+m8 <- 'F1 =~ ctar_q2_1 + ctar_q6_1 + ctar_q8_1 + ctar_q9_1 + ctar_q15_1 + ctar_q16_1
 
       # Setting the intercepts of the manifest variables to zero
       ctar_q2_1 ~ 0*1'
